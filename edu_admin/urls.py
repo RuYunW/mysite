2736c579e292a5995ea0653b5d6e4fb8b52ae23f
Ash <ruyunw@163.com>
2019-11-26 15:09:54 +0800

    adm

--- edu_admin/urls.py
+++ edu_admin/urls.py
@@ -2,17 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('welcome_Stu.html/', views.wel_stu, name='wel_stu'),
-    path('Stu_my_schedule.html/', views.stu_my_sch, name='stu_my_sch'),
-    path('Stu_select_sourses.html/', views.stu_select_src, name='stu_select_src'),
-    path('Stu_my_grade.html/', views.stu_my_grade, name='stu_my_grade'),
-    path('Stu_required_selected.html/', views.stu_req_selected, name='stu_req_select'),
+    path('Adm_manage_courses.html/', views.Adm_mng_cou, name='Adm_mng_cou'),
+    path('Adm_manage_students.html/', views.Adm_mng_stu, name='Adm_mng_stu'),
+    path('Adm_manage_teachers.html/', views.Adm_mng_tea, name='Adm_mng_tea'),
+    path('index.html/', views.index, name='index'),
     path('Stu_elective_selected.html/', views.stu_ele_select, name='stu_ele_select'),
+    path('Stu_modify_information.html/', views.stu_mod_info, name='stu_mod_info'),
+    path('Stu_my_grade.html/', views.stu_my_grade, name='stu_my_grade'),
+    path('Stu_my_schedule.html/', views.stu_my_sch, name='stu_my_sch'),
+    path('Stu_required_selected.html/', views.stu_req_selected, name='stu_req_select'),
+    path('Stu_select_sourses.html/', views.stu_select_src, name='stu_select_src'),
+    path('welcome_Adm.html/', views.wel_adm, name='wel_adm'),
+    path('welcome_Stu.html/', views.wel_stu, name='wel_stu'),
 
 
     # path('<int:stu_no>/', views.stu_info, name="stu_info"),
 
     # path('login.html/', views.user_login, name="login")
-    # path("", views.index, name='index')
+
 ]
--- edu_admin/views.py
+++ edu_admin/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Student
 
+def Adm_mng_cou(requset):
+    return render(requset, 'edu_admin/Adm_manage_courses.html', )
+
+def Adm_mng_stu(requset):
+    return render(requset, 'edu_admin/Adm_manage_students.html', )
+
+def Adm_mng_tea(requset):
+    return render(requset, 'edu_admin/Adm_manage_teachers.html', )
+
+def index(requset):
+    return render(requset, 'edu_admin/index.html', )
 
 def stu_name(request):
     stus = Student.objects.all()
     return render(request, "edu_admin/names.html", {"stus": stus})
-
 
 def stu_info(request, stu_no):
     # stu = Student.objects.get(stu_no=stu_no)
@@ -15,30 +25,31 @@ def stu_info(request, stu_no):
     sclass = stu.stu_class
     return render(request, "edu_admin/info.html", {"stu": stu, "school": school, "major": major, "sclass": sclass})
 
+def stu_ele_select(request):
+    return render(request, "edu_admin/Stu_elective_selected.html", )
+
+def stu_mod_info(requset):
+    return render(requset, 'edu_admin/Stu_modify_information.html', )
+
+def stu_my_sch(request):
+    return render(request, "edu_admin/Stu_my_schedule.html", )
+
+def stu_my_grade(request):
+    return render(request, "edu_admin/Stu_my_grade.html", )
+
+def stu_req_selected(request):
+    return render(request, "edu_admin/Stu_required_selected.html", )
+
+def stu_select_src(request):
+    return render(request, "edu_admin/Stu_select_sourses.html", )
+
+def wel_adm(requset):
+    return render(requset, 'edu_admin/welcome_Adm.html', )
 
 def wel_stu(request):
     # stu = get_object_or_404(Student, stu_no=stu_no)
     # name = stu.stu_name
     return render(request, "edu_admin/welcome_Stu.html", )
 
-
-def stu_my_sch(request):
-    return render(request, "edu_admin/Stu_my_schedule.html", )
-
-
-def stu_select_src(request):
-    return render(request, "edu_admin/Stu_select_sourses.html", )
-
-
-def stu_my_grade(request):
-    return render(request, "edu_admin/Stu_my_grade.html", )
-
-
-def stu_req_selected(request):
-    return render(request, "edu_admin/Stu_required_selected.html", )
-
-
-def stu_ele_select(request):
-    return render(request, "edu_admin/Stu_elective_selected.html", )
 
 
