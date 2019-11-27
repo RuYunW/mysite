0f70508b108a81dacec4b39b24c8ce52672e230f
Ash <ruyunw@163.com>
2019-11-27 10:08:24 +0800

    adm

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Account(models.Model):
@@ -10,5 +11,14 @@ class Account(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    major = models.CharField(max_length=50, null=False)
+
+    birth = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=20, null=True)
+
 
 
