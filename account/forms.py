c78e4ca1c4ba5103bb321a72bd195a065dbaa3aa
Ash <ruyunw@163.com>
2019-11-27 18:58:14 +0800

    adm

--- account/forms.py
+++ account/forms.py
@@ -8,11 +8,15 @@ class LoginForm(forms.Form):
 
 
 class RegistrationForm(forms.ModelForm):
-    name = forms.CharField(label="name", widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+    name = forms.CharField(label="name", widget=forms.TextInput(attrs={'placeholder': 'S', "class": "form-control", "id": "id", "style": "width: 200px"}))
     username = forms.CharField(label="username")
     password = forms.CharField(label="password", widget=forms.PasswordInput)
     school = forms.CharField(label="school")
     major = forms.CharField(label="major")
     sclass = forms.CharField(label="sclass")
+
+    class Meta:
+        model = User
+        fields = ("name", "username", "password", "school", "major", "sclass")
 
 
